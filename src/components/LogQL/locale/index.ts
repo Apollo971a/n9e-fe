f974c928f2a334316b974a5def0c61c32daf3f32
Apollo971 <thinkid2006@gmail.com>
2026-01-20 23:10:19 +0400

    add:  import ar_AE from './ar_AE';  Add Arabic localization support for LogQL
    add:  import ar_AE from './ar_AE';

--- src/components/LogQL/locale/index.ts
+++ src/components/LogQL/locale/index.ts
@@ -3,6 +3,8 @@ import zh_CN from './zh_CN';
 import zh_HK from './zh_HK';
 import ja_JP from './ja_JP';
 import ru_RU from './ru_RU';
+import ar_AE from './ar_AE';
+
 
 const resources = {
   logql: {
@@ -11,6 +13,7 @@ const resources = {
     zh_HK,
     ja_JP,
     ru_RU,
+    ar_AE,
   },
 };
 
