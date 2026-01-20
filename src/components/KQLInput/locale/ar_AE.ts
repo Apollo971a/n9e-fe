64fbcdc5b4cb53eecc9eed1d66ac2e8d6e61898e
Apollo971 <thinkid2006@gmail.com>
2026-01-20 23:06:26 +0400

    add:  ar_AE.ts Add Arabic locale for KQLInput component
    add:  ar_AE.ts

---
+++ src/components/KQLInput/locale/ar_AE.ts
@@ -0,0 +1,9 @@
+const ar_AE = {
+  fieldName: 'اسم الحقل',
+  fieldValue: 'قيمة الحقل',
+  combiningKeyword: 'كلمة بحث مركبة',
+  matchOperator: 'معامل المطابقة',
+  historicalRecords: 'السجلات التاريخية',
+  search: 'بحث',
+};
+export default ar_AE;
