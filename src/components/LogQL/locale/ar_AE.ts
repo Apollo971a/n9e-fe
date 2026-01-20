66783135ff6508936e665ed8db93d7b25aed01d6
Apollo971 <thinkid2006@gmail.com>
2026-01-20 23:11:36 +0400

    add:  ar_AE.ts  Add Arabic localization for LogQL
    add:  ar_AE.ts

---
+++ src/components/LogQL/locale/ar_AE.ts
@@ -0,0 +1,7 @@
+const ar_AE = {
+  logicalOperators: 'المعاملات المنطقية',
+  sqlkeywords: 'كلمات SQL المفتاحية',
+  fieldName: 'اسم الحقل',
+  historicalRecords: 'السجلات التاريخية',
+};
+export default ar_AE;
