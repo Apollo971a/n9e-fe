d50e0919672773782aa94d49545a42c38be6647a
Apollo971 <thinkid2006@gmail.com>
2026-01-20 23:16:50 +0400

    add: ar_AE.ts   Add Arabic localization for PromQLInput component
    add: ar_AE.ts

---
+++ src/components/PromQLInput/locale/ar_AE.ts
@@ -0,0 +1,10 @@
+const ar_AE = {
+  builder_btn: 'وضع المبتدئين',
+  builtinMetrics: {
+    btn: 'المقاييس المضمنة',
+    query_placeholder: 'يرجى إدخال اسم المقياس',
+  },
+  required: 'يرجى إدخال جملة استعلام PromQL',
+  placeholder: 'يرجى إدخال جملة استعلام PromQL',
+};
+export default ar_AE;
