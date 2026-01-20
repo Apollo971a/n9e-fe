a24552098509c51cedbe9721a970d99545e7944b
Apollo971 <thinkid2006@gmail.com>
2026-01-20 23:21:53 +0400

    add:  ar_AE.ts  Add Arabic localization for QueryName component
    add:  ar_AE.ts

---
+++ src/components/QueryName/locale/ar_AE.ts
@@ -0,0 +1,7 @@
+const ar_AE = {
+  tooltip: 'انقر لتعديل اسم الاستعلام',
+  required: 'يرجى إدخال اسم الاستعلام',
+  invalidName: 'يجب أن يبدأ بحرف، ويتكون من أحرف وأرقام وشرطات سفلية فقط',
+  duplicateName: 'هذا الاسم موجود بالفعل',
+};
+export default ar_AE;
