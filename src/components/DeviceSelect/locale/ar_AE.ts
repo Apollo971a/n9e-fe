239602b30d523782a18667f99b1d2127eb2b35a7
Apollo971 <thinkid2006@gmail.com>
2026-01-20 22:57:17 +0400

    add ar_AE.ts Add Arabic (AE) localization for DeviceSelect component
    add ar_AE.ts

---
+++ src/components/DeviceSelect/locale/ar_AE.ts
@@ -0,0 +1,23 @@
+const ar_AE = {
+  host: {
+    title: 'تصفية الأجهزة',
+    key: {
+      all_hosts: 'كافة الأجهزة',
+      group_ids: 'مجموعة العمل',
+      tags: 'العلامات',
+      hosts: 'معرف الجهاز',
+    },
+    preview: 'معاينة الأجهزة',
+  },
+  network_device: {
+    title: 'أجهزة الشبكة المفعلة',
+    key: {
+      all_devices: 'كافة أجهزة الشبكة',
+      group_ids: 'مجموعة العمل',
+      tags: 'العلامات',
+      ips: 'أجهزة الشبكة',
+    },
+    preview: 'معاينة أجهزة الشبكة',
+  },
+};
+export default ar_AE;
