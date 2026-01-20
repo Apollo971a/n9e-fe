ecf6205c88a78270dcda9b66c9795b97d22f455f
Apollo971 <thinkid2006@gmail.com>
2026-01-20 23:14:06 +0400

    add:  ar_AE.ts  Add Arabic localization for PromGraph component
    add:  ar_AE.ts

---
+++ src/components/PromGraphCpt/locale/ar_AE.ts
@@ -0,0 +1,6 @@
+const ar_AE = {
+  builder_btn: 'وضع المبتدئين',
+  query_btn: 'استعلام',
+  table_promql_interpolate_string: 'لقد اكتشفنا أن تعبير PromQL الخاص بك يحتوي على متغيرات، وتم استبدالها تلقائياً بـ 5m. تعبير PromQL بعد الاستبدال هو: {{query}}',
+};
+export default ar_AE;
