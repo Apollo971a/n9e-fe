afb158aeb77ee475c6addffc27fad4f30476dce8
Apollo971 <thinkid2006@gmail.com>
2026-01-20 23:03:12 +0400

    add: import ar_AE from './ar_AE'; Add Arabic localization for InputEnlarge component
    add: import ar_AE from './ar_AE';

--- src/components/InputEnlarge/locale/index.ts
+++ src/components/InputEnlarge/locale/index.ts
@@ -1,12 +1,14 @@
 import en_US from './en_US';
 import zh_HK from './zh_HK';
 import zh_CN from './zh_CN';
+import ar_AE from './ar_AE';
 
 const resources = {
   inputEnlarge: {
     en_US,
     zh_HK,
     zh_CN,
+    ar_AE,
   },
 };
 
