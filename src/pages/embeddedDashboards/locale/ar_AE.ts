a730700c09c1012fa83706966cb6d30d0b8abe2f
Apollo971 <thinkid2006@gmail.com>
2026-01-21 00:08:49 +0400

    add:  ar_AE.ts   Create ar_AE.ts
    add:  ar_AE.ts

---
+++ src/pages/embeddedDashboards/locale/ar_AE.ts
@@ -0,0 +1,11 @@
+const ar_AE = {
+  title: 'لوحة القيادة المدمجة',
+  edit_title: 'تعديل لوحة القيادة المدمجة',
+  edit_btn: 'إعداد الآن',
+  name: 'الاسم',
+  name_msg: 'يرجى إدخال الاسم',
+  url: 'الرابط',
+  url_msg: 'يرجى إدخال الرابط',
+  exitFullScreen_tip: 'لا يمكن التحكم في الخروج من ملء الشاشة بسبب قيود أمان iframe؛ يجب إزالة معامل viewMode=fullscreen من الرابط يدوياً',
+};
+export default ar_AE;
