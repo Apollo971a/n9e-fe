b50c3113dc744a460369f4ff650f10a641b41d5d
Apollo971 <thinkid2006@gmail.com>
2026-01-21 00:13:10 +0400

    add:  ar_AE.ts  Create ar_AE.ts
    add:  ar_AE.ts

---
+++ src/pages/embeddedProduct/locale/ar_AE.ts
@@ -0,0 +1,17 @@
+const ar_AE = {
+  title: 'تكامل النظام',
+  edit: 'تعديل',
+  add: 'إضافة',
+  name: 'الاسم',
+  name_msg: 'يرجى إدخال الاسم',
+  url: 'الرابط',
+  url_msg: 'يرجى إدخال الرابط',
+  visibility: 'الظهور',
+  visibility_msg: 'يرجى اختيار صلاحية الظهور',
+  login_user_visible: 'مرئي للمستخدمين المسجلين',
+  team_visible: 'مرئي لفرق محددة',
+  team_ids: 'مرئي لأعضاء الفرق التالية',
+  team_ids_msg: 'يرجى اختيار الفريق',
+  exitFullScreen_tip: 'الخروج من ملء الشاشة مقيد بسبب سياسات أمان iframe؛ يجب حذف معامل viewMode=fullscreen من الرابط يدوياً',
+};
+export default ar_AE;
