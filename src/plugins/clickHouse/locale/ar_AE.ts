378613c168d27f78ec7012ee72384cf92b44173d
Apollo971 <thinkid2006@gmail.com>
2026-01-21 11:43:05 +0400

    add: ar_AE.ts   Create ar_AE.ts
    add: ar_AE.ts

---
+++ src/plugins/clickHouse/locale/ar_AE.ts
@@ -0,0 +1,39 @@
+const ar_AE = {
+  preview: 'معاينة البيانات',
+  query: {
+    title: 'إحصائيات الاستعلام',
+    execute: 'استعلام',
+    query: 'شروط الاستعلام',
+    query_required: 'شروط الاستعلام لا يمكن أن تكون فارغة',
+    query_placeholder: 'أدخل SQL للاستعلام، واضغط على Shift+Enter لسطر جديد',
+    query_placeholder2: 'اضغط على Shift+Enter لسطر جديد',
+    advancedSettings: {
+      title: 'إعدادات مساعدة',
+      tags_placeholder: 'اضغط Enter لإدخال قيم متعددة',
+      valueKey: 'حقل القيم',
+      valueKey_tip: 'عادة ما تحتوي نتائج استعلام SQL على أعمدة متعددة؛ يمكنك تحديد الأعمدة التي سيتم عرض قيمها كمنحنيات في المخطط البياني',
+      valueKey_required: 'حقل القيم مطلوب',
+      labelKey: 'حقل العلامات (Labels)',
+      labelKey_tip: 'عادة ما تحتوي نتائج استعلام SQL على أعمدة متعددة؛ يمكنك تحديد الأعمدة التي ستعمل كمعلومات وصفية (Metadata) للعلامات في المنحنيات',
+    },
+    schema: 'معلومات الهيكل (Schema)',
+    document: 'وثائق الاستخدام',
+    dashboard: {
+      mode: {
+        label: 'وضع الاستعلام',
+        table: 'بيانات غير زمنية',
+        timeSeries: 'بيانات سلاسل زمنية',
+      },
+    },
+    historicalRecords: {
+      button: 'سجل المحفوظات',
+      searchPlaceholder: 'البحث في السجلات التاريخية',
+    },
+    compass_btn_tip: 'انقر لعرض بيانات الجدول',
+  },
+  trigger: {
+    title: 'شروط التنبيه',
+    value_msg: 'يرجى إدخال قيمة التعبير',
+  },
+};
+export default ar_AE;
