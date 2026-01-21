0008c0f83f7c5dbc5970b576b30a243db3c06476
Apollo971 <thinkid2006@gmail.com>
2026-01-21 12:44:04 +0400

    add:  import ar_AE from './ar_AE';  Update index.ts
    add:  import ar_AE from './ar_AE';

--- src/pages/monitor/object/locale/index.ts
+++ src/pages/monitor/object/locale/index.ts
@@ -1,12 +1,14 @@
 import en_US from './en_US';
 import zh_CN from './zh_CN';
 import zh_HK from './zh_HK';
+import ar_AE from './ar_AE';
 
 const resources = {
   objectExplorer: {
     en_US,
     zh_CN,
     zh_HK,
+    ar_AE,
   },
 };
 
