71493fd1a5266831aa080f7c21b328aaf958fd39
Apollo971 <thinkid2006@gmail.com>
2026-01-21 11:00:20 +0400

    add:  ar_AE.ts Create ar_AE.ts
    add:  ar_AE.ts

---
+++ src/pages/notFound/locale/ar_AE.ts
@@ -0,0 +1,6 @@
+const ar_AE = {
+    'The Flashcat webpage is': "صفحة ويب Flashcat",
+    'The Nightingale webpage is': "صفحة ويب مراقبة Nightingale",
+    'out of Service': "خارج الخدمة"
+};
+export default ar_AE;
