5916d3c0e40db3598edaef680d60e8a42d293c11
Apollo971 <thinkid2006@gmail.com>
2026-01-21 11:10:14 +0400

    add:  ar_AE.ts
    add:  ar_AE.ts

---
+++ src/pages/notificationTemplates/locale/ar_AE.ts
@@ -0,0 +1,27 @@
+const ar_AE = {
+  title: 'قوالب الرسائل',
+  add_title: 'إضافة قالب رسالة جديد',
+  edit_title: 'تعديل قالب الرسالة',
+  clone_title: 'نسخ قالب الرسالة',
+  user_group_ids: 'الفرق المصرح لها',
+  private: {
+    title: 'وضع العرض',
+    0: 'عام',
+    1: 'خاص',
+  },
+  notify_channel_ident: 'نوع الوسيط',
+  content: {
+    add_title: 'إضافة حقل للقالب',
+    edit_title: 'تعديل حقل القالب',
+    preview: 'معاينة محتوى القالب',
+    contentKey: 'معرف الحقل',
+    tip: 'الحقول المتاحة للاستخدام في وسائط الإشعارات، يمكن الإشارة إليها عبر $tpl.{{contentKey}}',
+    prompt: 'تم إجراء تعديلات على المحتوى، هل تريد تجاهل التغييرات؟',
+    value_msg: 'يرجى إدخال محتوى الحقل',
+  },
+  preview: {
+    select_events: 'اختيار أحداث التنبيه',
+    result: 'نتيجة المعاينة',
+  },
+};
+export default ar_AE;
