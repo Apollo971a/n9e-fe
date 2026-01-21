8e6b0190a253aed9b856b1eb2cdae3ed27977c52
Apollo971 <thinkid2006@gmail.com>
2026-01-21 10:34:51 +0400

    add:  ar_AE.ts  Create ar_AE.ts
    add:  ar_AE.ts

---
+++ src/pages/help/servers/locale/ar_AE.ts
@@ -0,0 +1,10 @@
+const ar_AE = {
+  title: 'محرك التنبيهات',
+  instance: 'نسخة المحرك (Instance)',
+  cluster: 'عنقود المحركات (Cluster)',
+  datasource: 'مصدر البيانات',
+  modify_datasource: 'تعديل مصدر البيانات',
+  clock: 'وقت آخر نبض استجابة (Heartbeat)',
+  unauthorized: 'ليس لديك صلاحية للعرض',
+};
+export default ar_AE;
