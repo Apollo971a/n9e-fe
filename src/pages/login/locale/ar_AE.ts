8415c8fe23f90047e0ea95fe71fbaafa4f477473
Apollo971 <thinkid2006@gmail.com>
2026-01-21 10:55:37 +0400

    add:  ar_AE.ts Create ar_AE.ts
    add:  ar_AE.ts

---
+++ src/pages/login/locale/ar_AE.ts
@@ -0,0 +1,13 @@
+const ar_AE = {
+  username: 'اسم المستخدم',
+  username_required: 'يرجى إدخال اسم المستخدم',
+  password: 'كلمة المرور',
+  password_required: 'يرجى إدخال كلمة المرور',
+  verifyvalue: 'رمز التحقق',
+  verifyvalue_required: 'يرجى إدخال رمز التحقق',
+  click_get_verify: 'انقر للحصول على رمز التحقق',
+  login: 'تسجيل الدخول',
+  other_types: 'طرق تسجيل دخول أخرى',
+  language: 'اللغة',
+};
+export default ar_AE;
