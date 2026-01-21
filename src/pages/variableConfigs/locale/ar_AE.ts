dc2d9656a732eb943fda8372df35c50e0d7fa7a7
Apollo971 <thinkid2006@gmail.com>
2026-01-21 11:32:52 +0400

    add:  ar_AE.ts  Create ar_AE.ts
    add:  ar_AE.ts

---
+++ src/pages/variableConfigs/locale/ar_AE.ts
@@ -0,0 +1,11 @@
+const ar_AE = {
+  title: 'قائمة المتغيرات',
+  search_placeholder: 'البحث عن طريق الاسم أو الملاحظات',
+  ckey: 'اسم المتغير',
+  cval: 'قيمة المتغير',
+  isEncrypted: 'هل هو مشفر؟',
+  resetPassword: 'إعادة تعيين كلمة المرور',
+  passwordNotSaved: 'لم يتم حفظ كلمة المرور',
+  passwordRequired: 'كلمة المرور مطلوبة',
+};
+export default ar_AE;
