b689260798830cd948b2f007cf5d2a61b9394293
Apollo971 <thinkid2006@gmail.com>
2026-01-21 11:14:54 +0400

    add:   ar_AE.ts
    add:   ar_AE.ts

---
+++ src/pages/permissions/locale/ar_AE.ts
@@ -0,0 +1,13 @@
+const ar_AE = {
+  title: 'إدارة الأدوار',
+  roles: 'قائمة الأدوار',
+  role_add: 'إضافة دور',
+  edit_title: {
+    post: 'إضافة دور',
+    put: 'تعديل الدور',
+  },
+  unselect_role: 'يرجى اختيار دور أولاً',
+  expand_all: 'توسيع الكل',
+  collapse_all: 'طي الكل',
+};
+export default ar_AE;
