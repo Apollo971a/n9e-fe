222a647556204ef58032e04c24897b087ef9ff95
Apollo971 <thinkid2006@gmail.com>
2026-01-21 12:18:45 +0400

    ar_AE.ts  Create ar_AE.ts
    add:  ar_AE.ts

---
+++ src/components/pageLayout/locale/ar_AE.ts
@@ -0,0 +1,13 @@
+const ar_AE = {
+  profile: 'الملف الشخصي',
+  themeSetting: 'إعدادات المظهر',
+  logout: 'تسجيل الخروج',
+  docs: 'الوثائق',
+  theme: {
+    title: 'تكوين المظهر',
+    title_help: 'الإعدادات تنطبق على المستخدم الحالي فقط',
+    sideMenu: 'القائمة الجانبية',
+    sideMenu_help: 'يعمل فقط في الوضع الفاتح',
+  },
+};
+export default ar_AE;
