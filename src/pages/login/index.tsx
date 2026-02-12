61b78f6d96355902be8534efa6d9654a167317fb
Apollo971 <thinkid2006@gmail.com>
2026-02-12 19:21:35 +0400

    add cybertrek.png logo Update index.tsx
    add cybertrek.png logo  at left top corner of login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -153,7 +153,10 @@ export default function Login() {
   };
 
   return (
-    <div className='login-warp'>
+//    <div className='login-warp'>
+    <div className='login-warp cybertrek-login'>
+      {/* Cybertrek Logo */}
+      <img src="/image/cybertrek.png" className="cybertrek-logo" />
       <div className='banner integration'>
         <img src={'/image/login-dashboard.svg'} style={{ margin: '0 60px', zIndex: 5, width: 632 }}></img>
       </div>
