122ccf6881606582f778030425e5d0dc0c855d53
Apollo971 <thinkid2006@gmail.com>
2026-02-13 10:49:25 +0400

    Update index.tsx
    delete:
    
    //      {/* Cybertrek top-left logo */}
    //      <img src="/image/cybertrek.png" className="logo integration slim" />

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -154,9 +154,6 @@ export default function Login() {
 
   return (
     <div className='login-warp'>
-
-//      {/* Cybertrek top-left logo */}
-//      <img src="/image/cybertrek.png" className="logo integration slim" />
       
       <div className='banner integration'>
         <img src={'/image/login-dashboard.svg'} style={{ margin: '0 60px', zIndex: 5, width: 632 }}></img>
